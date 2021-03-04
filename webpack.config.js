24f7417b41d2a21180fa69e981b8eefb1773fdfd
aidan <aidanguarniere@gmail.com>
2021-03-04 13:07:31 -0800

    project resturcture

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
-// requirements
+const webpack = require("webpack")
 const path = require("path");
 
 module.exports = {
-  // relative path to script.js
   entry: "./assets/js/script.js",
-  // output bundled code in main.bundle.js
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.bundle.js",
+    path: path.join(__dirname + "/dist"),
+    filename: "main.bundle.js"
   },
-  // do not run in production mode yet
-  mode: "development",
+  plugins: [
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery"
+  }),
+  ],
+  mode: "development"
 };
